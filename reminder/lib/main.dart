142f68e9f3bf820ec74ed73e10d5429a2cc677af
changoonetilleke <chanuka.goonetilleke@ontariotechu.net>
2022-10-17 22:30:15 -0400

    Connected firebase to our project

--- reminder/lib/main.dart
+++ reminder/lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:reminder/view/login.dart';
 import 'package:reminder/view/reminder_list.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
