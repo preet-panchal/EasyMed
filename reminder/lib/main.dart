f1c6cfa2aa5e2d1ed6bf09c7cf2cb402060d6dab
changoonetilleke <chanuka.goonetilleke@ontariotechu.net>
2022-10-18 22:47:18 -0400

    added the google sign in button with some functionality.

--- reminder/lib/main.dart
+++ reminder/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:reminder/view/google_sign_in.dart';
 import 'package:reminder/view/login.dart';
 import 'package:reminder/view/reminder_list.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -15,16 +17,16 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: LoginPage(),
-      routes: {
-        '/login' : (context) => ReminderList(),
-      },
-    );
-  }
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+      create: (context) => GoogleSignInProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+        ),
+        home: LoginPage(),
+        routes: {
+          '/login': (context) => ReminderList(),
+        },
+      ));
 }
