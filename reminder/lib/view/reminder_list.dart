6fe317c0a7dfb4536a0b4b7c691d8704dea340b1
changoonetilleke <chanuka.goonetilleke@ontariotechu.net>
2022-12-06 22:01:14 -0500

    design looks.

--- reminder/lib/view/reminder_list.dart
+++ reminder/lib/view/reminder_list.dart
@@ -39,26 +39,74 @@ class _ReminderListState extends State<ReminderList> {
     // TODO: implement build
     return Scaffold(
       appBar: navBar(context, "EasyMed", "Medications list pulled from Firebase."),
-      body: ListView.builder(
-          itemCount: _reminderList.length,
-          itemBuilder: (BuildContext context, int index) {
-            return GestureDetector(
-                child: Container(
-                    child: ListTile(
-              title: Text(_reminderList[index].time +
-                  " - " +
-                  _reminderList[index].name),
-              subtitle: Text(_reminderList[index].instructions),
-              selected: index == selectedIndex,
-              selectedTileColor: Colors.redAccent,
-              selectedColor: Colors.white,
-              onTap: () {
-                setState(() {
-                  selectedIndex = index;
-                });
-              },
-            )));
-          }),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height - 112,
+            child: ListView.builder(
+                itemCount: _reminderList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Stack(children: <Widget>[
+                    // The containers in the background
+                    Column(children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(left: 8.0, right: 8.0),
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 80.0,
+                          child: Padding(
+                            padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
+                            child: Material(
+                              color: Colors.white,
+                              elevation: 14.0,
+                              shadowColor: Color(0x802196F3),
+                              child: Center(
+                                child: Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                    children: <Widget>[
+                                      Text(
+                                        _reminderList[index].name,
+                                        style: TextStyle(
+                                            color: Colors.red,
+                                            fontSize: 20.0),
+                                      ),
+                                      Column(
+                                        mainAxisAlignment:
+                                        MainAxisAlignment.center,
+                                        children: <Widget>[
+                                          Text(
+                                            _reminderList[index].date,
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 18.0,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          Text(
+                                            _reminderList[index].time,
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 16.0),
+                                          ),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ]),
+                  ]);
+                }),
+          ),
+        ],
+      ),
     );
   }
 
